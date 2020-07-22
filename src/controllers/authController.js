ddc4d754f70cc0eaffbcc8f2c0bf47167077c573
Gilberto da Silva Raimundo <Gilberto.Raimundo@gs1br.org>
2020-07-22 11:28:54 -0300

    API NodeJS + Express + Mongo - Autenticação

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth.json');
 
 const User = require('../models/user');
 
 const router = express.Router();
 
+function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {    // Token Generate
+        expiresIn: 86400,
+    });
+}
+
+// Start Register
 router.post('/register', async (req, res) => {
     const { email } = req.body;
 
@@ -15,10 +26,34 @@ router.post('/register', async (req, res) => {
 
         user.password = undefined;
 
-        return res.send({ user });
+        return res.send({
+            user,
+            token: generateToken({ id: user.id }),
+        });
     }   catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
+});
+
+// Start authentication
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user)
+    return res.status(400).send({ error: 'User not found' });
+
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Invalid password' });
+
+    user.password = undefined;
+
+        res.send({
+            user,
+            token: generateToken({ id: user.id }),
+        });
+
 });
 
 module.exports = app => app.use('/auth', router);
