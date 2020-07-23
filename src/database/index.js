635336dc83e7d1760452f4833f6021573dd7c6f7
Gilberto da Silva Raimundo <Gilberto.Raimundo@gs1br.org>
2020-07-23 08:16:45 -0300

    API NodeJS + Express + Mongo - Recuperação de senha com NodeMailer

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/noderest', {
     useNewUrlParser:true,
     useUnifiedTopology:true,   
     useCreateIndex:true, 
+    useFindAndModify:false
 })
 .then(response => console.log('Conected to Database..'))
 .catch(error => console.log('error ->', error.message));
-mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
